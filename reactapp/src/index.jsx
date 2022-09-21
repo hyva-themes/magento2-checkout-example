fdab2c54a299536f25394ccc53d0ff9b959cccdf
Rajeev K Tomy <rajeevphpdeveloper@gmail.com>
2022-09-21 18:13:28 +0530

    port 1.1.2 release

--- reactapp/src/index.jsx
+++ reactapp/src/index.jsx
@@ -4,7 +4,6 @@ import { createRoot } from 'react-dom/client';
 
 import RootElement from '@hyva/react-checkout/utils/rootElement';
 import CheckoutForm from '@hyva/react-checkout/components/CheckoutForm';
-import StepProvider from '@hyva/react-checkout/context/Form/Step/StepProvider';
 import AppDataProvider from '@hyva/react-checkout/context/App/AppDataProvider';
 import CartDataProvider from '@hyva/react-checkout/context/Cart/CartDataProvider';
 import CheckoutFormProvider from '@hyva/react-checkout/context/Form/CheckoutFormProvider';
@@ -16,9 +15,7 @@ function Checkout() {
     <AppDataProvider>
       <CartDataProvider>
         <CheckoutFormProvider>
-          <StepProvider>
-            <CheckoutForm />
-          </StepProvider>
+          <CheckoutForm />
         </CheckoutFormProvider>
       </CartDataProvider>
     </AppDataProvider>
